3f8d218888065761e0bc8495156fc85acc231153
Rakhi <rakhi158agrahari@gmail.com>
2024-07-13 10:47:14 +0530

    dfd

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ console.log('learning git');
 console.log('Version 1');
 console.log(" new line")
 
-// 
+// git log 
 // commit 8c7937105e83282f08738a28a35a7d4b769abb7c
 // Author: Rakhi <rakhi158agrahari@gmail.com>
 // Date:   Sat Jul 13 09:55:18 2024 +0530
+console.log("git commit -modify ")
+ console.log('git status - We can show the changes & modified text in the File ');   
+ console.log('git  - We can show the changes in the File '); 
+
+//  git remote is use for the work on git 
+ console.log('git add remote origin git@github.com:Rakhi-Agrahari123/Git_repo.git')  
